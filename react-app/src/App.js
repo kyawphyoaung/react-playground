b2f1ea1a090cffc94c2a8da2f2338a35f9515aa3
MollyMoe <aprilnikee@gmail.com>
2023-05-13 03:28:31 +0630

    render props

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,63 +1,35 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useState, useEffect } from "react";
 
-const query = `
-  query {
-    allLifts{
-      name
-      elevationGain
-      status
-    }
-  }
-`;
+const moutain_data = [
+  { name: "Freel", elevation: 10891 },
+  { name: "Monument", elevation: 10067 },
+  { name: "Pyramid", elevation: 9983 },
+  { name: "Tallac", elevation: 9735 },
+];
 
-const opts = {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ query }),
-};
-
-function Lift({ name, elevationGain, status }) {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <p>
-        {elevationGain} {status}
-      </p>
-    </div>
+function List({ data, renderItem, renderEmpty }) {
+  return !data.length ? (
+    renderEmpty
+  ) : (
+    <ul>
+      {data.map((item) => (
+        <li key={data.name}>{renderItem(item)}</li>
+      ))}
+    </ul>
   );
 }
 
 function App() {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setLoading(true);
-    fetch(`https://snowtooth.moonhighway.com/`, opts)
-      .then((response) => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .then(setError);
-  }, []);
-
-  if (loading) return <h1>Loading.....</h1>;
-
-  if (error) return <pre>{JSON.stringify(error)}</pre>;
-
-  if (!data) return null;
   return (
-    <div>
-      {data.data.allLifts.map((lift) => (
-        <Lift
-          name={lift.name}
-          elevationGain={lift.elevationGain}
-          status={lift.status}
-        />
-      ))}
-    </div>
+    <List
+      data={moutain_data}
+      renderEmpty={<p> The data is empty</p>}
+      renderItem={(item) => (
+        <>
+          {item.name} - {item.elevation}ft.
+        </>
+      )}
+    />
   );
 }
 
