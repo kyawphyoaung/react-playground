0a135bdca65e20a04549991792acc4369d1dc55b
MollyMoe <aprilnikee@gmail.com>
2023-05-12 22:06:21 +0630

    creating controlled from elements

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useRef } from "react";
+import {useState} from "react";
+
 
 function App({ library }) {
-
-  const txtTitle = useRef();
-  const hexColor = useRef();
+  const [title,setTitle ] = useState("");
+  const [color,setColor ] = useState("#000000");
 
   const submit = (e) =>{
     e.preventDefault();
-    const title = txtTitle.current.value;
-    const color = hexColor.current.value;
     alert(`${title}, ${color}`);
-    txtTitle.current.value = "";
-    hexColor.current.value = "";
+    setTitle("");
+    setColor("#ffffff");    
   }
 
   return (
     <form onSubmit={submit}>
-      <input ref={txtTitle} type="text" placeholder="Color Title" />
-      <input ref={hexColor} type="color" />
+      <input value={title} onChange={(event)=> setTitle(event.target.value)}  type="text" placeholder="Color Title" />
+      <input value={color} onChange={(event)=> setColor(event.target.value)} type="color" />
       <button> ADD </button>
     </form> 
   );
